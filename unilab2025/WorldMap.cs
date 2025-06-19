3c692340283beac233acbac8f3866a8e913cb8be
ryotaiijima <ring2510ring@gmail.com>
2025-06-19 11:16:38 +0900

    クリア処理

--- unilab2025/WorldMap.cs
+++ unilab2025/WorldMap.cs
@@ -15,12 +15,18 @@ namespace unilab2025
         public WorldMap()
         {
             InitializeComponent();
-            
+
+            this.KeyDown += new KeyEventHandler(WorldMap_KeyDown);
+            this.KeyPreview = true;
+
         }
 
         #region 読み込み時
         private void WorldMap_Load(object sender, EventArgs e)
         {
+            if (!(ClearCheck.IsCleared[1, 0])) button2.Visible = false;
+            if (!(ClearCheck.IsCleared[2, 0])) button3.Visible = false;
+            if (!(ClearCheck.IsCleared[3, 0])) button4.Visible = false;
             if (!(ClearCheck.IsCleared[4,0])) button5.Visible = false;
         }
         #endregion
@@ -82,5 +88,42 @@ namespace unilab2025
         {
             Func.CreateAnotherWorld(this);
         }
+
+        #region クリアチェックスキップ用
+        private void WorldMap_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.M)
+            {
+                for (int i = 1; i <= 4; i++)
+                {
+                    for (int j = 0; j < (int)ConstNum.numStages; j++)
+                    {
+                        ClearCheck.IsNew[i, j] = false;
+                        ClearCheck.IsCleared[i, j] = true;
+                        ClearCheck.IsButtonEnabled[i, j] = true;
+                    }
+                }
+
+                ClearCheck.PlayAfterChapter4Story = true;
+
+                for (int i = 5; i < (int)ConstNum.numWorlds; i++)
+                {
+                    for (int j = 0; j <= 1; j++)
+                    {
+                        ClearCheck.IsNew[i, j] = true;
+                        ClearCheck.IsButtonEnabled[i, j] = true;
+                    }
+                }
+
+                Func.CreateWorldMap(this);
+            }
+        }
+        #endregion
+
+
+
+
+
+
     }
 }
