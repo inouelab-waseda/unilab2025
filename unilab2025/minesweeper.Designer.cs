1fc3724928e5553864b50ecc74362d5e127b7615
ryotaiijima <ring2510ring@gmail.com>
2025-07-18 17:58:49 +0900

    マインスイーパー作り途中1

---
+++ unilab2025/minesweeper.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace unilab2025
+{
+    partial class minesweeper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_Start = new System.Windows.Forms.Button();
+            this.button_Reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(235, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(861, 679);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_Start
+            // 
+            this.button_Start.Location = new System.Drawing.Point(1124, 391);
+            this.button_Start.Name = "button_Start";
+            this.button_Start.Size = new System.Drawing.Size(182, 92);
+            this.button_Start.TabIndex = 1;
+            this.button_Start.Text = "スタート";
+            this.button_Start.UseVisualStyleBackColor = true;
+            this.button_Start.Click += new System.EventHandler(this.button_Start_Click);
+            // 
+            // button_Reset
+            // 
+            this.button_Reset.Location = new System.Drawing.Point(1124, 200);
+            this.button_Reset.Name = "button_Reset";
+            this.button_Reset.Size = new System.Drawing.Size(182, 93);
+            this.button_Reset.TabIndex = 2;
+            this.button_Reset.Text = "リセット";
+            this.button_Reset.UseVisualStyleBackColor = true;
+            // 
+            // minesweeper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1348, 721);
+            this.Controls.Add(this.button_Reset);
+            this.Controls.Add(this.button_Start);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "minesweeper";
+            this.Text = "minesweeper";
+            this.Load += new System.EventHandler(this.minesweeper_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_Start;
+        private System.Windows.Forms.Button button_Reset;
+    }
+}
---
+++ unilab2025/minesweeper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace unilab2025
+{
+    public partial class minesweeper : Form
+    {
+        public minesweeper()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+
+        public static int[,] map=new int [20,20];
+        public static int[,] mine = new int[20, 20];
+        public static int[,] flag = new int[20, 20];
+
+
+        private void minesweeper_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Start_Click(object sender, EventArgs e)
+        {
+            int NMine = 20 * 20/10;
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    map[i, j] = 0;
+                }
+            }
+            Random rnd = new Random();
+            while (true)
+            {
+                
+                int random_x = rnd.Next(0, 20);
+                int random_y = rnd.Next(0, 20);
+                if (map[random_x, random_y]==0)
+                {
+                    map[random_x, random_y] = 1;
+                    NMine -= 1;
+                    if (NMine == 0) break;
+                }
+
+            }
+
+        }
+
+
+
+
+    }
+}
