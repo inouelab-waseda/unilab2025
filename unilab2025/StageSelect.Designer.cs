918a7cbed04526c67cf71311e4c8f974b355386e
ryotaiijima <ring2510ring@gmail.com>
2025-07-18 17:00:44 +0900

    ステージ選択画面更新

--- unilab2025/StageSelect.Designer.cs
+++ unilab2025/StageSelect.Designer.cs
@@ -30,9 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StageSelect));
             this.button_ToMap = new System.Windows.Forms.Button();
-            this.button_Stage1 = new unilab2025.CustomButton();
-            this.button_Stage2 = new unilab2025.CustomButton();
             this.button_Stage3 = new unilab2025.CustomButton();
+            this.button_Stage2 = new unilab2025.CustomButton();
+            this.button_Stage1 = new unilab2025.CustomButton();
             this.SuspendLayout();
             // 
             // button_ToMap
@@ -49,24 +49,25 @@
             this.button_ToMap.UseVisualStyleBackColor = false;
             this.button_ToMap.Click += new System.EventHandler(this.button_ToMap_Click);
             // 
-            // button_Stage1
+            // button_Stage3
             // 
-            this.button_Stage1.BackColor = System.Drawing.Color.Transparent;
-            this.button_Stage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button_Stage1.ConditionImage = null;
-            this.button_Stage1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button_Stage1.ForeImage = null;
-            this.button_Stage1.Location = new System.Drawing.Point(117, 565);
-            this.button_Stage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button_Stage1.Name = "button_Stage1";
-            this.button_Stage1.Size = new System.Drawing.Size(469, 128);
-            this.button_Stage1.TabIndex = 2;
-            this.button_Stage1.UseVisualStyleBackColor = false;
-            this.button_Stage1.Click += new System.EventHandler(this.button_StageI_Click);
+            this.button_Stage3.BackColor = System.Drawing.Color.Transparent;
+            this.button_Stage3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_Stage3.ConditionImage = null;
+            this.button_Stage3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_Stage3.ForeImage = null;
+            this.button_Stage3.Location = new System.Drawing.Point(1180, 565);
+            this.button_Stage3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_Stage3.Name = "button_Stage3";
+            this.button_Stage3.Size = new System.Drawing.Size(469, 128);
+            this.button_Stage3.TabIndex = 4;
+            this.button_Stage3.UseVisualStyleBackColor = false;
+            this.button_Stage3.Click += new System.EventHandler(this.button_StageI_Click);
             // 
             // button_Stage2
             // 
             this.button_Stage2.BackColor = System.Drawing.Color.Transparent;
+            this.button_Stage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button_Stage2.ConditionImage = null;
             this.button_Stage2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button_Stage2.ForeImage = null;
@@ -78,20 +79,20 @@
             this.button_Stage2.UseVisualStyleBackColor = false;
             this.button_Stage2.Click += new System.EventHandler(this.button_StageI_Click);
             // 
-            // button_Stage3
+            // button_Stage1
             // 
-            this.button_Stage3.BackColor = System.Drawing.Color.Transparent;
-            this.button_Stage3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.button_Stage3.ConditionImage = null;
-            this.button_Stage3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.button_Stage3.ForeImage = null;
-            this.button_Stage3.Location = new System.Drawing.Point(1180, 565);
-            this.button_Stage3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button_Stage3.Name = "button_Stage3";
-            this.button_Stage3.Size = new System.Drawing.Size(469, 128);
-            this.button_Stage3.TabIndex = 4;
-            this.button_Stage3.UseVisualStyleBackColor = false;
-            this.button_Stage3.Click += new System.EventHandler(this.button_StageI_Click);
+            this.button_Stage1.BackColor = System.Drawing.Color.Transparent;
+            this.button_Stage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button_Stage1.ConditionImage = null;
+            this.button_Stage1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_Stage1.ForeImage = null;
+            this.button_Stage1.Location = new System.Drawing.Point(117, 565);
+            this.button_Stage1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button_Stage1.Name = "button_Stage1";
+            this.button_Stage1.Size = new System.Drawing.Size(469, 128);
+            this.button_Stage1.TabIndex = 2;
+            this.button_Stage1.UseVisualStyleBackColor = false;
+            this.button_Stage1.Click += new System.EventHandler(this.button_StageI_Click);
             // 
             // StageSelect
             // 
