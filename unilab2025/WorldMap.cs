5e732ae443ec10eb6b0194c6dff7353cbd9ee6d5
ryotaiijima <ring2510ring@gmail.com>
2025-08-04 19:20:15 +0900

    マップnewの更新

--- unilab2025/WorldMap.cs
+++ unilab2025/WorldMap.cs
@@ -78,11 +78,7 @@ namespace unilab2025
                         if (ClearCheck.IsNew[i, 0])
                         {
                             if (i != 5) button.ConditionImage = Dictionaries.Img_Button["New"];
-                            else if (i == 5 && MainNew.isNew) 
-                            { 
-                                button.ConditionImage = Dictionaries.Img_Button["New"];
-                                MainNew.isNew=false;
-                            }
+                            
 
 
                         }
@@ -99,6 +95,11 @@ namespace unilab2025
                     }
 
                 }
+            }
+            if (ClearCheck.IsCleared[4, 0] && MainNew.isNew)
+            {
+                button5.ConditionImage = Dictionaries.Img_Button["New"];
+                
             }
 
             // 画面遷移後に再生する会話があったら再生
@@ -136,6 +137,7 @@ namespace unilab2025
 
         private void button5_Click(object sender, EventArgs e)
         {
+            MainNew.isNew = false;
             Func.CreateAnotherWorld(this);
         }
 
